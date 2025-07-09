07acde486c0b9ed99566f109372d8a6553f8d0aa
bondiano <batbondik0@gmail.com>
2025-07-09 18:34:56 +0700

    test: add more test and use lib facade

--- test/map_reduce_test.exs
+++ test/map_reduce_test.exs
@@ -3,11 +3,12 @@ defmodule MapReduce.WorkerTest do
 
   use ExUnit.Case, async: true
 
+  alias MapReduce
   alias MapReduce.Worker
 
   describe "create/0" do
     test "creates a worker process" do
-      worker = Worker.create()
+      worker = MapReduce.create()
 
       assert %Worker{pid: pid, monitor_ref: ref} = worker
       assert is_pid(pid)
@@ -16,12 +17,12 @@ defmodule MapReduce.WorkerTest do
     end
 
     test "executes multiple jobs with different job_ids" do
-      worker = Worker.create()
+      worker = MapReduce.create()
       job1 = fn -> 1 + 1 end
       job2 = fn -> 2 + 2 end
 
-      job_id1 = Worker.execute(worker, job1)
-      job_id2 = Worker.execute(worker, job2)
+      job_id1 = MapReduce.execute(worker, job1)
+      job_id2 = MapReduce.execute(worker, job2)
 
       assert job_id1 != job_id2
     end
@@ -29,40 +30,40 @@ defmodule MapReduce.WorkerTest do
 
   describe "get_result/2" do
     test "gets result of completed job" do
-      worker = Worker.create()
+      worker = MapReduce.create()
       job = fn -> 42 end
 
-      job_id = Worker.execute(worker, job)
-      result = Worker.get_result(worker, job_id)
+      job_id = MapReduce.execute(worker, job)
+      result = MapReduce.get_result(worker, job_id)
 
       assert result == {:ok, 42}
     end
 
     test "handles job errors" do
-      worker = Worker.create()
+      worker = MapReduce.create()
       job = fn -> raise "Test error" end
 
-      job_id = Worker.execute(worker, job)
-      result = Worker.get_result(worker, job_id)
+      job_id = MapReduce.execute(worker, job)
+      result = MapReduce.get_result(worker, job_id)
 
       assert {:error, %RuntimeError{message: "Test error"}} = result
     end
 
     test "multiple callers can get same result" do
-      worker = Worker.create()
+      worker = MapReduce.create()
 
       job = fn ->
         Process.sleep(50)
         :shared_result
       end
 
-      job_id = Worker.execute(worker, job)
+      job_id = MapReduce.execute(worker, job)
 
       parent = self()
 
       for i <- 1..3 do
         spawn_link(fn ->
-          result = Worker.get_result(worker, job_id)
+          result = MapReduce.get_result(worker, job_id)
           send(parent, {:result, i, result})
         end)
       end
@@ -82,27 +83,27 @@ defmodule MapReduce.WorkerTest do
 
   describe "reduce/3" do
     test "reduces empty list" do
-      worker = Worker.create()
+      worker = MapReduce.create()
       jobs = []
       associative_func = fn a, b -> a + b end
 
-      result = Worker.reduce(worker, jobs, associative_func)
+      result = MapReduce.reduce(worker, jobs, associative_func)
 
       assert result == {:ok, nil}
     end
 
     test "reduces single job" do
-      worker = Worker.create()
+      worker = MapReduce.create()
       jobs = [fn -> 42 end]
       associative_func = fn a, b -> a + b end
 
-      result = Worker.reduce(worker, jobs, associative_func)
+      result = MapReduce.reduce(worker, jobs, associative_func)
 
       assert result == {:ok, 42}
     end
 
     test "reduces jobs with lists" do
-      worker = Worker.create()
+      worker = MapReduce.create()
 
       jobs = [
         fn -> [1, 2] end,
@@ -112,17 +113,24 @@ defmodule MapReduce.WorkerTest do
 
       associative_func = fn a, b -> Enum.concat(a, b) end
 
-      result = Worker.reduce(worker, jobs, associative_func)
+      result = MapReduce.reduce(worker, jobs, associative_func)
 
       assert result == {:ok, [1, 2, 3, 4, 5, 6]}
     end
 
+    test "reduce with multiple jobs" do
+      worker = MapReduce.create()
+      jobs = [fn -> 1 end, fn -> 2 * 2 end, fn -> 3 * 3 end, fn -> 4 * 4 end]
+      result = MapReduce.reduce(worker, jobs, fn a, b -> a + b end)
+      assert result == {:ok, 30}
+    end
+
     test "stop reduce on one error" do
-      worker = Worker.create()
+      worker = MapReduce.create()
       jobs = [fn -> 1 end, fn -> raise "Test error" end, fn -> 3 end]
       associative_func = fn a, b -> a + b end
 
-      result = Worker.reduce(worker, jobs, associative_func)
+      result = MapReduce.reduce(worker, jobs, associative_func)
 
       assert result == {:error, %RuntimeError{message: "Test error"}}
     end
@@ -130,7 +138,7 @@ defmodule MapReduce.WorkerTest do
 
   describe "concurrent operations" do
     test "handles concurrent job execution" do
-      worker = Worker.create()
+      worker = MapReduce.create()
       parent = self()
 
       for i <- 1..5 do
@@ -140,8 +148,8 @@ defmodule MapReduce.WorkerTest do
             i * 10
           end
 
-          job_id = Worker.execute(worker, job)
-          result = Worker.get_result(worker, job_id)
+          job_id = MapReduce.execute(worker, job)
+          result = MapReduce.get_result(worker, job_id)
           send(parent, {:completed, i, result})
         end)
       end
@@ -161,21 +169,21 @@ defmodule MapReduce.WorkerTest do
 
   describe "error handling" do
     test "handles job that throws" do
-      worker = Worker.create()
+      worker = MapReduce.create()
       job = fn -> throw(:test_throw) end
 
-      job_id = Worker.execute(worker, job)
-      result = Worker.get_result(worker, job_id)
+      job_id = MapReduce.execute(worker, job)
+      result = MapReduce.get_result(worker, job_id)
 
       assert {:error, {:throw, :test_throw}} = result
     end
 
     test "handles job that exits" do
-      worker = Worker.create()
+      worker = MapReduce.create()
       job = fn -> exit(:test_exit) end
 
-      job_id = Worker.execute(worker, job)
-      result = Worker.get_result(worker, job_id)
+      job_id = MapReduce.execute(worker, job)
+      result = MapReduce.get_result(worker, job_id)
 
       assert {:error, {:exit, :test_exit}} = result
     end
